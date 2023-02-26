ad49de1ad858ba188a1e4eac3553ea7f3186bb88
Swaroop Hegde <email@swaroophegde.com>
2023-02-25 17:53:03 -0800

    added steps to auto start with default values - tested with deploy build-dev

--- init_processes.sh
+++ init_processes.sh
@@ -5,7 +5,16 @@ if [ ! -f "settings/settings.json" ]; then
     exit 1;
 fi
 
+if [ -z "$UUID" ]; then
+    echo "UUID not found in .env - autopopulating to 'generated-uuid'!";
+    export uuid="${UUID:-generated-uuid}"
+    echo "UUID set to $uuid";
+    #exit 1;
+fi
+
 echo 'starting processes...';
 pm2 start pm2.config.js
+
+poetry run python -m snapshotter_cli add-snapshotter "{\"rate_limit\": \"6000000/day;10000/minute;600/second\", \"uuid\": \"$uuid\", \"active\": \"active\", \"name\": \"generated 1\", \"email\": \"generated1@example.com\", \"alias\": \"generated-1\"}";
 
 pm2 logs --lines 1000
