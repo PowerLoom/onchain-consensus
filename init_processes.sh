ab1b85a4de6edb05c2bf731c97eeeab52f246a2f
Swaroop Hegde <email@swaroophegde.com>
2023-03-07 00:42:07 -0800

    added name and removed debug exit

--- init_processes.sh
+++ init_processes.sh
@@ -15,15 +15,17 @@ fi
 
 export uuid="${UUID:-generated-uuid}"
 echo "UUID set to $uuid";
+export name="${NAME:-generated-name}"
+echo "Name set to $name";
 export alias="${ALIAS:-generated-alias}"
 echo "Alias set to $alias";
 export email="${EMAIL:-generated@example.com}"
 echo "Email set to $email";
-exit;
+echo "{\"rate_limit\": \"6000000/day;10000/minute;600/second\", \"uuid\": \"$uuid\", \"active\": \"active\", \"name\": \"$name\", \"email\": \"$email\", \"alias\": \"$alias\"}";
 
 echo 'starting processes...';
 pm2 start pm2.config.js
 
-poetry run python -m snapshotter_cli add-snapshotter "{\"rate_limit\": \"6000000/day;10000/minute;600/second\", \"uuid\": \"$uuid\", \"active\": \"active\", \"name\": \"generated 1\", \"email\": \"$email\", \"alias\": \"$alias\"}";
+poetry run python -m snapshotter_cli add-snapshotter "{\"rate_limit\": \"6000000/day;10000/minute;600/second\", \"uuid\": \"$uuid\", \"active\": \"active\", \"name\": \"$name\", \"email\": \"$email\", \"alias\": \"$alias\"}";
 
 pm2 logs --lines 1000
